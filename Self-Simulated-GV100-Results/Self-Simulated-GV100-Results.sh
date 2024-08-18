3f4830a89ce86b76b6f852ad104c90c0a1004895
ConvolutedDog <102723346+ConvolutedDog@users.noreply.github.com>
2024-08-18 10:15:39 +0800

    Update Self-Simulated-GV100-Results.sh

--- Self-Simulated-GV100-Results/Self-Simulated-GV100-Results.sh
+++ Self-Simulated-GV100-Results/Self-Simulated-GV100-Results.sh
@@ -510,7 +510,7 @@ do
 done
 mpirun -np 10 $CONFIG1 $TRACES_SAVE_RELATIVE_PATH/Tango/LSTM/configs --kernel_id 0 $CONFIG2
 python3 ./merge_report.py --dir $TRACES_SAVE_PATH/Tango/LSTM/outputs --kernel_id 0 --np 10
-for kernel_id in {0..99}
+# for kernel_id in {0..99}
 # do
 # 	mpirun -np 10 $CONFIG1 $TRACES_SAVE_RELATIVE_PATH/Tango/ResNet/configs --kernel_id $kernel_id $CONFIG2
 # 	python3 ./merge_report.py --dir $TRACES_SAVE_PATH/Tango/ResNet/outputs --kernel_id $kernel_id --np 10
